6b2c008c607c128f13a94664ad6c3620f6bbffba
Dominic Mullen <dmullen17@gmail.com>
2018-07-13 15:29:35 -0700

    rt update / added aon email

--- R/login_rt.R
+++ R/login_rt.R
@@ -36,5 +36,34 @@ rt::rt_login(user = RT_USER,
              pass = RT_PASS,
              rt_base = RT_URL)
 
+
 ## Write this function 
 check_rt_login()
+
+rt::rt_ticket_search(query = "Queue:'arcticAwards'AND(Status='new')", rt_base = "https://support.nceas.ucsb.edu/rt")
+
+base_api <- rt:::rt_url(rt_base, "search", "ticket?")
+
+#based on httr::modify_url()
+#possible TODO - turn this into its own function that can be used internally in the package
+inputs <- rt:::compact(list(query = utils::URLencode(query, reserved = TRUE),
+                       orderBy = orderBy,
+                       format = format))
+
+params <- paste(paste0(names(inputs), "=", inputs), collapse = "&")
+
+url <- paste0(base_api, params)
+
+out <- rt:::rt_GET(url)
+
+if(format == "s"){
+  out$content <- out$content %>%
+    tidyr::gather(id, value)
+}
+
+if(format == "i"){
+  out$content <- out$content %>%
+    stringr::str_split("\n")
+}
+
+return(out)
