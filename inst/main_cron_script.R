4531dff35685f0276643f915066d98beae0c5043
Christopher Jones <csj@christopherjones.co>
2018-11-15 10:46:35 -0700

    Use install_github without interaction

--- inst/main_cron_script.R
+++ inst/main_cron_script.R
@@ -19,20 +19,21 @@ library(stringr)
 library(XML)
 library(devtools)
 
-## Set up environment variables 
-readRenviron(file.path(getwd(), '.Renviron'))
-
 # Install rt separately
 if (!("rt" %in% installed.packages()[,"Package"]) ) {
     # Last stable version since this is so far unreleased
-    remotes::install_github('NCEAS/rt@a9fe278ac0b74cf9ef3912c01d1e1559e1816eae')}
+    remotes::install_github('NCEAS/rt@a9fe278ac0b74cf9ef3912c01d1e1559e1816eae', 
+        upgrade = "never")}
 library(rt)
 
 ## Source the bot
 if (!("awardsBot" %in% installed.packages()[,"Package"]) ) {
-    remotes::install_github('NCEAS/awards-bot@1.0.3')
+    remotes::install_github('NCEAS/awards-bot@1.0.3', upgrade = "never")
 }
 library(awardsBot)
+
+## Set up environment variables 
+readRenviron(file.path(getwd(), '.Renviron'))
 
 # Log in to RT/SLACK 
 rt::rt_login(Sys.getenv('RT_USER'), Sys.getenv('RT_PASS'), Sys.getenv('RT_URL'))
