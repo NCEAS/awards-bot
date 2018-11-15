2654e91ae506b8b501076d3547ec7efe47a05296
Christopher Jones <csj@christopherjones.co>
2018-11-15 10:32:07 -0700

    Fix cron script issues and dependencies
    The main cron script was calling the `awardsBot::main()` method, instead 
    use the un-exported call.
    
    To deploy and run the script, library dependencies need to first be 
    satisfied. Check for missing libraries and install them.

--- inst/main_cron_script.R
+++ inst/main_cron_script.R
@@ -1,23 +1,40 @@
+# Check for installed dependencies
+packages <- 
+    c("dplyr", "lubridate", "magrittr", "RCurl", "slackr", "stringr", "XML", "devtools")
+new_packages <- packages[!(packages %in% installed.packages()[,"Package"])]
+
+# Install unsatisfied dependencies
+if(length(new_packages)) {
+    install.packages(new_packages)
+}
 library(dplyr)
 library(httr)
 library(lubridate)
 library(magrittr)
 library(RCurl)
-library(rt)
 library(slackr)
 library(stringr)
 library(XML)
+library(devtools)
 
 ## Set up environment variables 
 readRenviron(file.path(getwd(), '.Renviron'))
 
+# Install rt separately
+if (!("rt" %in% installed.packages()[,"Package"]) ) {
+    # Last stable version since this is so far unreleased
+    remotes::install_github('NCEAS/rt@a9fe278ac0b74cf9ef3912c01d1e1559e1816eae')}
+library(rt)
+
 ## Source the bot
-remotes::install_github('NCEAS/awards-bot')
+if (!("awardsBot" %in% installed.packages()[,"Package"]) ) {
+    remotes::install_github('NCEAS/awards-bot')
+}
 library(awardsBot)
 
 # Log in to RT/SLACK 
 rt::rt_login(Sys.getenv('RT_USER'), Sys.getenv('RT_PASS'), Sys.getenv('RT_URL'))
 slackr::slackr_setup(channel = '#awardbot', username = 'awardbot',
-                     incoming_webhook_url = Sys.getenv('SLACK_WEBHOOK_URL'))
+    incoming_webhook_url = Sys.getenv('SLACK_WEBHOOK_URL'))
 
-awardsBot::main()
+awardsBot:::main()
