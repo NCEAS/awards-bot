0a73661cd97fab149b1a785566c692dbb798630e
laijasmine <jasminelai@nceas.ucsb.edu>
2021-05-14 14:58:26 -0700

    more formatting tweaks

--- R/RT_check_correspondence.R
+++ R/RT_check_correspondence.R
@@ -1,4 +1,4 @@
-#' Get recent incoming correspondence formatted
+#' Get recent incoming correspondence
 #'
 #' @param ticket_id (character) the RT ticket number
 #' @param after (character) check all the correspondences sent after this date
@@ -28,10 +28,9 @@ get_recent_incoming_correspondence <- function(ticket_id, after) {
 
     # get the specific correspondence
     id <- stringr::str_extract(inc, "[0-9]*")
-    
+
     response <- rt::rt_ticket_history_entry(ticket_id, id)
 
-    # !!! check if this works
     if (response$Created <= after) {
       next
     }
@@ -42,39 +41,47 @@ get_recent_incoming_correspondence <- function(ticket_id, after) {
   return(correspondences)
 }
 
+#' Format the slack message
+#'
+#' @param msg (character) an rt_api object with the response
+#' @param trunc_at (numeric) the length of the correspondence to include in the slack message
+#'
+#' @return (character) formatted slack message text
+#' @export
+#'
+#' @examples
 format_history_entry <- function(msg, trunc_at = 200) {
   if (nchar(msg["Content"]) > trunc_at) {
     ellipsis <- "..."
   } else {
     ellipsis <- ""
   }
-  
+
   if (msg["Type"] == "Correspond") {
     msg["Type"] <- "Correspondence"
   } else if (msg["Type"] == "Create") {
     msg["Type"] <- "Ticket created"
   }
-  
-  #construct the slack message
+
+  # construct the slack message
   sprintf(
     "%s by %s on <%s/Ticket/Display.html?id=%s|Ticket %s>:\n>%s%s",
-    msg$Type, msg$Creator,
-    Sys.getenv("RT_BASE_URL"),
+    msg$Type, msg$Creator, Sys.getenv("RT_BASE_URL"),
     msg$Ticket, msg$Ticket,
-    strtrim(msg$Content, 200),
+    strtrim(msg$Content, trunc_at),
     ellipsis
   )
 }
 
 #' Get incoming correspondences
-#' modified from the submissions bot python code
+#' modified from the submissions-bot python code
 #'
 #' @param after (character) date YYYY-MM-DD
 #'
 #' @return
 #' @export
 #'
-#' @examples 
+#' @examples
 get_tickets_with_new_incoming_correspondence <- function(after) {
   tickets <- rt::rt_ticket_search(paste0("Queue='arcticAwards' AND LastUpdated >'", after, " 00:00:00'"))
 
--- tests/testthat/test_RT_check_correspondence.R
+++ tests/testthat/test_RT_check_correspondence.R
@@ -11,6 +11,8 @@ test_that('we can get all the correspondences for all the tickets', {
   tickets <- get_tickets_with_new_incoming_correspondence("2021-05-03")
   expect_equal(length(tickets), 20)
   
+  tic <- get_tickets_with_new_incoming_correspondence("2021-05-06")
+  
   ticket_none <- get_tickets_with_new_incoming_correspondence("2021-05-11")
   expect_null(ticket_none)
 
